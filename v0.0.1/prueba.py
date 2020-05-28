ae78ba29f557f02beed165d305602504132bf2df
kahto <maggiolo.agustin@hotmail.com>
2020-05-27 22:17:45 -0300

    prueba de ventana

--- v0.0.1/prueba.py
+++ v0.0.1/prueba.py
@@ -1,2 +1,18 @@
-esta es la primera prueba del respositorio
-a ver si funciona esto
+import pygame
+
+
+
+#inicializo pygame
+pygame.init()
+
+
+#creo la pantalla
+screen = pygame.display.set_mode((800,600))  # aca pongo el ancho y alto de la ventana
+
+
+#creo el loop del programa
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # aca le estoy diciendo si aprento la x pare cerrar cambie el valor de running
+            running = FALSE
