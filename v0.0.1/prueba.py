bed3dcbef7a99534ecb36aa7cde8a9babe8bf119
kahto <maggiolo.agustin@hotmail.com>
2020-05-30 17:21:41 -0300

    Prolijidad

--- v0.0.1/prueba.py
+++ v0.0.1/prueba.py
@@ -1,30 +1,30 @@
-import pygame
+import sys,Pygame
 
-#inicializo pygame
-pygame.init()
-
-
-#creo la pantalla
-screen = pygame.display.set_mode((800,600))  # aca pongo el ancho y alto de la ventana
-
-
-#titulo e icono de ventana
-pygame.display.set_caption("projectp")  # aca pongo el nombre de la ventana
-icon= pygame.image.load('war.png')  # aca pongo el nombre del archivo del icono
-pygame.display.set_icon(icon)
+#Inicializamos Pygamep
+pygame.init_()
+#Ventana
+size= 800,800                               # paso tamaño de la ventana
+screen =pygame.display.set_mode(size)
+#Titulo de la Ventana
+pygame.display.set_caption("Project P")
 
 
-#creo el loop del programa
-running = True
-while running:
+#Bucle juego
+
+run=True
+
+while run:
+    #Capturamos los eventos que se han producido
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # aca le estoy diciendo si aprento la x pare cerrar cambie el valor de running
-            running = False
 
 
 
 
-#relleno fondo pantalla RGB - Red,Gren,Blue
 
-screen.fill((192,192,192))
-pygame.display.update()
+
+        #si el evento es salir de la ventana termina
+        if event.type == pygame.QUIT run  = False
+
+#Salida
+pygame.quit()
