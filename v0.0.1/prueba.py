7bd3d4b31b6ec1235e1bf1a3ac5a7bf329d9a60b
kahto <maggiolo.agustin@hotmail.com>
2020-05-30 16:28:57 -0300

    subo archivo de clases

--- v0.0.1/prueba.py
+++ v0.0.1/prueba.py
@@ -21,6 +21,9 @@ while running:
         if event.type == pygame.QUIT:  # aca le estoy diciendo si aprento la x pare cerrar cambie el valor de running
             running = False
 
+
+
+
 #relleno fondo pantalla RGB - Red,Gren,Blue
 
 screen.fill((192,192,192))
