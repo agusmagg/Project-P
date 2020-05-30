d2c6d68a6280156dd4440db9421bb624e1f8ccdf
kahto <maggiolo.agustin@hotmail.com>
2020-05-30 16:00:14 -0300

    aca subo los assets micro roguelike

--- v0.0.1/prueba.py
+++ v0.0.1/prueba.py
@@ -1,7 +1,5 @@
 import pygame
 
-
-
 #inicializo pygame
 pygame.init()
 
@@ -10,9 +8,20 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))  # aca pongo el ancho y alto de la ventana
 
 
+#titulo e icono de ventana
+pygame.display.set_caption("projectp")  # aca pongo el nombre de la ventana
+icon= pygame.image.load('war.png')  # aca pongo el nombre del archivo del icono
+pygame.display.set_icon(icon)
+
+
 #creo el loop del programa
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # aca le estoy diciendo si aprento la x pare cerrar cambie el valor de running
-            running = FALSE
+            running = False
+
+#relleno fondo pantalla RGB - Red,Gren,Blue
+
+screen.fill((192,192,192))
+pygame.display.update()
